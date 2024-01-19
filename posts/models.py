0bf7c9601bb649e31a8f94344d15d64b9daa3eee
oleg_zubkov <zoleg1030@gmail.com>
2024-01-19 18:17:36 +0300

    add new models and update views-function and templates

--- posts/models.py
+++ posts/models.py
@@ -4,6 +4,14 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
+class Group(models.Model):
+    title = models.CharField(max_length=30)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
 class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
@@ -12,3 +20,10 @@ class Post(models.Model):
         on_delete=models.CASCADE,
         related_name='posts'
     )
+    group = models.ForeignKey(
+        Group,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+    )
+
--- posts/views.py
+++ posts/views.py
@@ -1,22 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Post, Group
 
 
 def index(request):
-    template = 'posts/index.html'
-    text = 'Это главная страница проекта Blissful'
+    posts = Post.objects.order_by('-pub_date')[:10]
+    title = 'Последние обновления на сайте'
     context = {
-        'text': text,
+        'posts': posts,
+        'title': title,
     }
-    return render(request, template, context)
+    return render(request, 'posts/index.html', context)
 
-def group_list(request):
-    template = 'posts/group_list.html'
-    text = 'Здесь будет информация о группах проекта Blissful'
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     context = {
-        'text': text,
+        'group': group,
+        'posts': posts,
     }
-    return render(request, template, context)
+    return render(request, 'posts/group_list.html', context)
 
-def post_detail(request, pk):
-    return HttpResponse(f'{pk}')
