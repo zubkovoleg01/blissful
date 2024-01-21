6a1c22ce9cfaacc95311d03230a16c4f80473e8a
oleg_zubkov <zoleg1030@gmail.com>
2024-01-21 14:34:35 +0300

    add pagination

--- posts/views.py
+++ posts/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.core.paginator import Paginator
 from .models import Post, Group
 
 
 def index(request):
-    posts = Post.objects.order_by('-pub_date')[:10]
-    title = 'Последние обновления на сайте'
+    post_list = Post.objects.all().order_by('-pub_date')
+
+    paginator = Paginator(post_list, 10)
+
+    page_number = request.GET.get('page')
+
+    page_obj = paginator.get_page(page_number)
     context = {
-        'posts': posts,
-        'title': title,
+        'page_obj': page_obj,
     }
     return render(request, 'posts/index.html', context)
 
