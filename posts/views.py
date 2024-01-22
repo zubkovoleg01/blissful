fef0fce3f029d306d1b2337efebb1580b8690807
oleg_zubkov <zoleg1030@gmail.com>
2024-01-22 16:08:56 +0300

    create profile and post_detail pages

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.contrib.auth import get_user_model
 from django.core.paginator import Paginator
 from .models import Post, Group
 
@@ -27,3 +28,29 @@ def group_posts(request, slug):
     }
     return render(request, 'posts/group_list.html', context)
 
+def profile(request, username):
+    user_posts = Post.objects.filter(author__username=username)
+    c_posts = user_posts.count()
+    paginator = Paginator(user_posts, 10)
+
+    page_number = request.GET.get('page')
+
+    page_obj = paginator.get_page(page_number)
+
+    context = {
+        'username': username,
+        'c_posts': c_posts,
+        'user_posts': user_posts,
+        'page_obj': page_obj,
+    }
+    return render(request, 'posts/profile.html', context)
+
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    c_posts = Post.objects.filter(author=post.author).count()
+    context = {
+        'post': post,
+        'c_posts': c_posts,
+    }
+    return render(request, 'posts/post_detail.html', context)
